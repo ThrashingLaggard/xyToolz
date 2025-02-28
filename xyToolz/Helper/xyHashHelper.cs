66bb716b8f7ab467efd295d896050a1ed458c583
Hansi <bl@ticmed.de>
2025-02-28 17:35:20 +0100

    Samples integriert

--- xyToolz/Helper/xyHashHelper.cs
+++ xyToolz/Helper/xyHashHelper.cs
@@ -27,3 +27,46 @@ namespace xyToolz.Helper
 
     }
 }
+
+//public class Passwords
+//{
+//      private const int SaltSize = 16; // 128-bit
+//      private const int KeySize = 32;  // 256-bit
+//      private const int Iterations = 10000;
+
+//      /// <summary>
+//      /// 
+//      /// </summary>
+//      /// <param name="password"></param>
+//      /// <returns></returns>
+//      public static string HashPassword ( string password )
+//      {
+//            byte [ ] salt = RandomNumberGenerator.GetBytes (SaltSize);
+
+//            using var pbkdf2 = new Rfc2898DeriveBytes (password, salt, Iterations, HashAlgorithmName.SHA256);
+//            byte [ ] hash = pbkdf2.GetBytes (KeySize);
+//            return Convert.ToBase64String (salt) + ":" + Convert.ToBase64String (hash);
+//      }
+
+//      /// <summary>
+//      /// 
+//      /// </summary>
+//      /// <param name="password"></param>
+//      /// <param name="hashedPassword"></param>
+//      /// <returns></returns>
+//      public static bool VerifyPassword ( string password, string hashedPassword )
+//      {
+//            string [ ] parts = hashedPassword.Split (':');
+//            if (parts.Length != 2)
+//            {
+//                  return false;
+//            }
+
+//            byte [ ] salt = Convert.FromBase64String (parts [ 0 ]);
+//            byte [ ] hash = Convert.FromBase64String (parts [ 1 ]);
+
+//            using var pbkdf2 = new Rfc2898DeriveBytes (password, salt, Iterations, HashAlgorithmName.SHA256);
+//            byte [ ] hashToCheck = pbkdf2.GetBytes (KeySize);
+//            return CryptographicOperations.FixedTimeEquals (hash, hashToCheck);
+//      }
+//}
