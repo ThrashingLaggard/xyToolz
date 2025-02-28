ad9251b409c57be2e384c50e118dd6b46f221739
Hansi <bl@ticmed.de>
2025-02-28 17:33:27 +0100

    Hash-Helper in Toolbox integriert

---
+++ xyToolz/Helper/xyHashHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xyToolz.Helper
+{
+      public static class xyHashHelper
+      {
+            public static string Pepper { get; set; }
+            private const int SaltLength = 16;  // 128 Bit
+            private const int KeyLenth = 32;    // 256 Bit
+            private const int Iterations = 1000;
+
+            public static string HashPassword(string password)
+            {
+                  string hashedPassword = "";
+                  byte [ ] salt = RandomNumberGenerator.GetBytes (SaltLength);
+
+
+
+
+                  return hashedPassword;
+            }
+
+    }
+}
