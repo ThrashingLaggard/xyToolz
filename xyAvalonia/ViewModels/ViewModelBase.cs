ef178d15c277b2866c7642fc76dec468cb9d7a3a
Hansi <test@lol.de>
2025-03-02 12:37:56 +0100

    xyAvalonia erblickt das Licht der Welt

---
+++ xyAvalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace xyAvalonia.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ xyAvalonia/Views/DebugConsole.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace xyAvalonia;
+
+public partial class DebugConsole : Window
+{
+    public DebugConsole()
+    {
+        InitializeComponent();
+    }
+}
