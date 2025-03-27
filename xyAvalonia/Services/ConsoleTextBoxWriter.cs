3d92b4977595f6db5ae09f4537884309c36c51f1
Hansi <test@lol.de>
2025-03-27 13:01:58 +0100

    Standardoutput abgreifen?!

---
+++ xyAvalonia/Services/ConsoleTextBoxWriter.cs
@@ -0,0 +1,48 @@
+﻿using Avalonia.Controls;
+using Avalonia.Threading;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using xyToolz.Helper.Logging;
+
+namespace xyAvalonia.Services
+{
+    public class ConsoleTextBoxWriter : TextWriter
+    {
+        private readonly TextBox _output;
+
+        public ConsoleTextBoxWriter(TextBox output)
+        {
+            _output = output;
+        }
+
+        public override void Write(string value)
+        {
+            Dispatcher.UIThread.Post
+            (
+                () =>
+                    {
+                        _output.Text += value + Environment.NewLine;
+                        _output.CaretIndex = _output.Text.Length;
+                        _output.ScrollToLine(_output.CaretIndex);
+                    }
+            );
+        }
+
+
+
+
+
+
+        private static String FormatMsgForLogs(String message) => xyLogFormatter.FormatMessageForLogging(message);
+        private static String FormatExMsgForLogs(Exception exception) => xyLogFormatter.FormatExceptionDetails(exception, Microsoft.Extensions.Logging.LogLevel.Error);
+
+
+        public override Encoding Encoding => Encoding.UTF8;
+    }
+}
--- xyAvalonia/Views/DebugConsole.axaml.cs
+++ xyAvalonia/Views/DebugConsole.axaml.cs
@@ -3,13 +3,15 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Avalonia.Threading;
-
+using Microsoft.VisualStudio.TestPlatform.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-
+using xyAvalonia.Services;
+using xyAvalonia.Views;
 using xyToolz;
 using xyToolz.Helper.Logging;
 
@@ -17,137 +19,180 @@ namespace xyAvalonia;
 
 public partial class DebugConsole : Window
 {
-      public event Action<String> LogMessageReceived;
 
-      public void OnMessageReceived ( String message , [CallerMemberName] String? callerName = null )
-      {
-            LogMessageReceived?.Invoke (( message ));
-      }
-      public DebugConsole ( )
-      {
-            xyLog.LogMessageSent +=  LogMessageReceived ;
-          
-            InitializeComponent ( );
-      }
+    private TextWriter _consoleWriter;
 
-      private static String FormatMsgForLogs ( String message ) => xyLogFormatter.FormatMessageForLogging (message);
-      private static String FormatExMsgForLogs ( Exception exception ) => xyLogFormatter.FormatExceptionDetails (exception , Microsoft.Extensions.Logging.LogLevel.Error);
+    public event Action<String> LogMessageReceived;
+    public void OnMessageReceived(String message, [CallerMemberName] String? callerName = null)
+    {
+        LogMessageReceived?.Invoke((message));
+    }
 
-      /// <summary>
-      /// Text zur GUI hinzufügen
-      /// </summary>
-      /// <param name="message"></param>
-      public void AppendText ( String message )
-      {
-            Dispatcher.UIThread.InvokeAsync (( ) =>
+
+
+    public DebugConsole()
+    {
+        InitializeComponent();
+        xyLog.LogMessageSent += LogMessageReceived;
+        _consoleWriter = new ConsoleTextBoxWriter(ConsoleOutput);
+        Console.SetOut(_consoleWriter);
+        Console.SetError(_consoleWriter);
+    }
+
+    public async Task ReadProgramOutput(string programPath)
+    {
+        MainWindow mainWindow = (MainWindow)this.Owner;
+        mainWindow.Hide();
+        
+        Process process = new Process
+        {
+            StartInfo = new ProcessStartInfo
             {
-                  ConsoleOutput.Text += message + Environment.NewLine;
-                  ConsoleOutput.CaretIndex = ConsoleOutput.Text.Length;
-                  ScrollToEnd ( );
-            });
-      }
-
-      /// <summary>
-      /// Immer die neuesten Ausgaben
-      /// </summary>
-      private void ScrollToEnd ( )
-      {
-            var scrollViewer = this.FindControl<ScrollViewer> ("ScrollViewer");
-            if (scrollViewer != null)
-            {
-                  scrollViewer.Offset = new Vector (0 , scrollViewer.Extent.Height);
+                FileName = programPath,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
             }
-      }
+        };
 
-      /// <summary>
-      /// Submit yout input
-      /// </summary>
-      /// <param name="sender"></param>
-      /// <param name="e"></param>
-      private void AddButton_Click ( Object sender , RoutedEventArgs e )
-      {
-            string input = InputTextBox.Text!;
-            if (!string.IsNullOrWhiteSpace (input))
-            {
-                  var formatted = xyLogFormatter.FormatMessageForLogging (input);
-                  AppendText (formatted);
-                  InputTextBox.Text = string.Empty; // Eingabefeld leeren
-            }
-      }
+        process.OutputDataReceived += async (sender, e) => await xyLog.AsxLog(e.Data!);
+        process.ErrorDataReceived += async (sender, e) =>
+         {
+             if (!string.IsNullOrEmpty(e.Data))
+             {
+                 Exception exception = new Exception(e.Data);
+                 await xyLog.AsxExLog(exception);
+             }
+         };
 
-      /// <summary>
-      /// Clear the Debug Console
-      /// </summary>
-      /// <param name="sender"></param>
-      /// <param name="e"></param>
-      private void BtnClear_Click ( Object sender , RoutedEventArgs e )
-      {
-            ConsoleOutput.Text = String.Empty;
-            //xyQOL.Crash(9999);
-      }
+        process.Start();
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
 
-      private void BtnExport_Click ( Object sender , RoutedEventArgs e )
-      {
-            string? outputText = ConsoleOutput.Text;
-            try
-            {
-                  using var _ = WriteIntoFile (outputText!);
-            }
-            catch (Exception ex)
-            {
-                  xyLog.ExLog (ex);
-            }
+        await process.WaitForExitAsync();
+        mainWindow.Show();
+    }
 
 
-      }
-      private async Task WriteIntoFile ( String outputText )
-      {
-            // Beispiel: Speichern in eine Textdatei
-            var saveFileDialog = new SaveFileDialog
-            {
-                  Title = "Export Console Output" ,
-                  Filters = new List<FileDialogFilter>
+
+
+    /// <summary>
+    /// Submit yout input
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void AddButton_Click(Object sender, RoutedEventArgs e)
+    {
+        string input = InputTextBox.Text!;
+        if (!string.IsNullOrWhiteSpace(input))
+        {
+            var formatted = xyLogFormatter.FormatMessageForLogging(input);
+            _consoleWriter.Write(formatted);
+            InputTextBox.Text = string.Empty; // Eingabefeld leeren
+        }
+    }
+
+
+    /// <summary>
+    /// Clear the Debug Console
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void BtnClear_Click(Object sender, RoutedEventArgs e)
+    {
+        ConsoleOutput.Text = String.Empty;
+        //xyQOL.Crash(9999);
+    }
+
+    private void BtnExport_Click(Object sender, RoutedEventArgs e)
+    {
+        string? outputText = ConsoleOutput.Text;
+        try
+        {
+            using var _ = WriteIntoFile(outputText!);
+        }
+        catch (Exception ex)
+        {
+            xyLog.ExLog(ex);
+        }
+    }
+
+
+    private async Task WriteIntoFile(String outputText)
+    {
+        // Beispiel: Speichern in eine Textdatei
+        var saveFileDialog = new SaveFileDialog
+        {
+            Title = "Export Console Output",
+            Filters = new List<FileDialogFilter>
         {
             new FileDialogFilter { Name = "Text Files", Extensions = { "txt" } },
             new FileDialogFilter { Name = "All Files", Extensions = { "*" } }
         }
-            };
+        };
 
-            var result = await saveFileDialog.ShowAsync (this);
-            if (result != null)
-            {
-                  File.WriteAllText (result , outputText);
-            }
+        var result = await saveFileDialog.ShowAsync(this);
+        if (result != null)
+        {
+            File.WriteAllText(result, outputText);
+        }
 
-      }
+    }
 
-      /// <summary>
-      /// Delete the placehoder when clicking on it
-      /// </summary>
-      /// <param name="sender"></param>
-      /// <param name="e"></param>
-      private void InputTextBox_GotFocus ( Object sender , RoutedEventArgs e )
-      {
-            if (InputTextBox.Text == "Please enter your billing info here...")
-            {
-                  InputTextBox.Text = string.Empty;
-            }
-      }
+    /// <summary>
+    /// Delete the placehoder when clicking on it
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void InputTextBox_GotFocus(Object sender, RoutedEventArgs e)
+    {
+        if (InputTextBox.Text == "Please enter your billing info here...")
+        {
+            InputTextBox.Text = string.Empty;
+        }
+    }
 
-      /// <summary>
-      /// Put the placeholder back in...
-      /// </summary>
-      /// <param name="sender"></param>
-      /// <param name="e"></param>
-      private void InputTextBox_LostFocus ( Object sender , RoutedEventArgs e )
-      {
-            if (string.IsNullOrWhiteSpace (InputTextBox.Text))
-            {
-                  InputTextBox.Text = "Please enter your billing info here...";
-            }
-      }
+    /// <summary>
+    /// Put the placeholder back in...
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void InputTextBox_LostFocus(Object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(InputTextBox.Text))
+        {
+            InputTextBox.Text = "Please enter your billing info here...";
+        }
+    }
 
 
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+    /// <summary>
+    /// Text zur GUI hinzufügen
+    /// </summary>
+    /// <param name="message"></param>
+    public void AppendText(String message)
+    {
+        Dispatcher.UIThread.InvokeAsync(() =>
+        {
+            ConsoleOutput.Text += message + Environment.NewLine;
+            ConsoleOutput.CaretIndex = ConsoleOutput.Text.Length;
+            ScrollToEnd();
+        });
+    }
+
+    /// <summary>
+    /// Immer die neuesten Ausgaben
+    /// </summary>
+    private void ScrollToEnd()
+    {
+        var scrollViewer = this.FindControl<ScrollViewer>("ScrollViewer");
+        if (scrollViewer != null)
+        {
+            scrollViewer.Offset = new Vector(0, scrollViewer.Extent.Height);
+        }
+    }
 
 }
