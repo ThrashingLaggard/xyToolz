20c4d5df765d210ea9b7da0e0488e5f7fc5779d1
Hansi <test@lol.de>
2025-05-24 02:15:24 +0200

    Adding Interfaces for generic DB Operations& Model QOL

---
+++ xyToolz/Database/Interfaces/IBaseCrud.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xyToolz.Database.Interfaces
+{
+    public interface IBaseCrud<T> where T : class
+    {
+        Task<bool> Create(T Entity);
+        Task<T?> Read<T>(int id);
+        Task<bool> Update(T Entity);
+        Task<bool> Delete(T Entity);
+    }
+}
---
+++ xyToolz/Database/Interfaces/IEfCoreCrudExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace xyToolz.Database.Interfaces
+{
+    /// <summary>
+    /// Providing EF Core extensions for IBaseCrud
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IEfCoreCrudExtensions<T> where T : class
+    {
+        Task<EntityEntry> GetEntryByID(int id);
+    }
+}
