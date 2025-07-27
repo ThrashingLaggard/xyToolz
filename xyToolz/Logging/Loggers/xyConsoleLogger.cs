54993a7d39690bd24f2d1c2a94c44c7230ddac77
Hansi <test@lol.de>
2025-07-27 19:42:45 +0200

    fix: Workflow works

--- xyToolz/Logging/Loggers/xyConsoleLogger.cs
+++ xyToolz/Logging/Loggers/xyConsoleLogger.cs
@@ -11,7 +11,7 @@ namespace xyToolz.Logging.Loggers
 {
     public class xyConsoleLogger<T> : ILogging
     {
-        // Almost irrelevant, but not utterly                                                                                                                                        TODO: Add methods o digest xyLogEntrys
+        // Almost irrelevant, but not utterly.                                                                                                                                      TODO: Add methods o digest xyLogEntrys
         private readonly IMessageFormatter? _msgFormatter;
         private readonly IExceptionFormatter? _excFormatter;
         private readonly IEntityFormatter<T>? _entFormatter;
