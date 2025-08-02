21ee3186dfc24ce2c07412a842cf9e416e7e907a
Hansi <test@lol.de>
2025-08-02 15:30:14 +0200

    fix: i need to update the workflow

--- xyToolz/Logging/Helper/xyMessageFactory.cs
+++ xyToolz/Logging/Helper/xyMessageFactory.cs
@@ -1,12 +1,4 @@
-﻿using PdfSharp.Pdf.Content.Objects;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Diagnostics.Contracts;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics.CodeAnalysis;
 
 namespace xyToolz.Logging.Helper
 {
