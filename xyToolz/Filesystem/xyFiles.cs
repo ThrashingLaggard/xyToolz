63ca910383a1cdc05e36ce74c32f12a27c984b8c
Hansi <test@lol.de>
2025-08-30 18:38:38 +0200

    deactivated utf8 BOM in SaveToFile()

--- xyToolz/Filesystem/xyFiles.cs
+++ xyToolz/Filesystem/xyFiles.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -560,7 +561,7 @@ namespace xyToolz.Filesystem
 
             try
             {
-                await File.WriteAllTextAsync(filePath, content);
+                await File.WriteAllTextAsync(filePath, content, new UTF8Encoding(false));
                 await xyLog.AsxLog(saveSuccessMsg + $" {filePath}");
                 return true;
             }
