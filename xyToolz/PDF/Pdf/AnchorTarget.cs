e3ed756cf734ea94b19e778c06c7ff6dcebb8be5
Hansi <test@lol.de>
2026-01-19 23:54:23 +0100

    fix: Removed the stuff from xyDocumentor for now need, another solution

--- xyToolz/PDF/Pdf/AnchorTarget.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace xyToolz.Pdf
-{
-    using PdfSharpCore.Pdf;
-
-    /// <summary>
-    /// Value object for anchor destinations inside the PDF.
-    /// </summary>
-    public readonly record struct AnchorTarget(PdfPage Page, double Y);
-}
