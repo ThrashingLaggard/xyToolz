54ea8e23f033af08785ae885eb9daa03ae0f66cd
Hansi <test@lol.de>
2026-01-25 21:32:21 +0100

    Outsourcing

--- Misc/xyConversion.cs
+++ Misc/xyConversion.cs
@@ -1,6 +1,4 @@
-﻿using System.Text;
-using xyToolz.QOL;
-using xyToolz.StaticLogging;
+﻿using xyToolz.QOL;
 
 namespace xyToolz.Misc
 {
@@ -361,84 +359,5 @@ namespace xyToolz.Misc
                   Console.WriteLine(result);
                   return result;
             }
-
-        #region Encoding – String and Byte Conversion
-
-        /// <summary>
-        /// Converts a UTF-8 string into a byte array.
-        /// </summary>
-        /// <param name="input">The UTF-8 string to convert.</param>
-        /// <returns>A byte array representation of the input string.</returns>
-        public static byte[] StringToBytes(string input)
-        {
-            try
-            {
-                return Encoding.UTF8.GetBytes(input);
-            }
-            catch (Exception ex)
-            {
-                xyLog.ExLog(ex);
-                return null!;
-            }
-        }
-
-        /// <summary>
-        /// Converts a Base64-encoded string into a byte array.
-        /// </summary>
-        /// <param name="base64">The Base64-encoded input string.</param>
-        /// <returns>The decoded byte array.</returns>
-        public static byte[] BaseToBytes(string base64)
-        {
-            try
-            {
-                return Convert.FromBase64String(base64);
-            }
-            catch (Exception ex)
-            {
-                xyLog.ExLog(ex);
-                return null!;
-            }
-        }
-
-        /// <summary>
-        /// Converts a byte array into a UTF-8 string.
-        /// </summary>
-        /// <param name="bytes">The input byte array.</param>
-        /// <returns>A UTF-8 string representation of the byte array.</returns>
-        public static string BytesToString(byte[] bytes)
-        {
-            try
-            {
-                return Encoding.UTF8.GetString(bytes);
-            }
-            catch (Exception ex)
-            {
-                xyLog.ExLog(ex);
-                return null!;
-            }
-        }
-
-        /// <summary>
-        /// Converts a byte array into a Base64-encoded string.
-        /// </summary>
-        /// <param name="bytes">The input byte array.</param>
-        /// <returns>A Base64 string representation of the byte array.</returns>
-        public static string BytesToBase(byte[] bytes)
-        {
-            try
-            {
-                return Convert.ToBase64String(bytes);
-            }
-            catch (Exception ex)
-            {
-                xyLog.ExLog(ex);
-                return null!;
-            }
-        }
-
-
-
-        #endregion
-
     }
 }
