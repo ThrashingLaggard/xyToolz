b4c8dc52af110e987add9df62bdd8d28e9987188
Hansi <test@lol.de>
2026-02-05 01:07:20 +0100

    Added new helper class for DateTime operations

---
+++ xyToolz/DateTime/xyDateTime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xyToolz.TimeDate
+{
+    public class xyDateTime
+    {
+        /// <summary>
+        /// Get the hash code for 'DateTime.Now()'
+        /// </summary>
+        /// <returns>int hashCode</returns>
+        public static int GetHashFromNow() => DateTime.Now.GetHashCode();
+    }
+}
