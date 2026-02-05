402dd7c1ade90b6cbfa9e98b4ed738a88b1228da
Hansi <test@lol.de>
2026-02-05 02:35:32 +0100

    Added a class to spill collections, either with or without whitespace

---
+++ xyToolz/QOL/xySpiller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xyToolz.QOL
+{
+    public class xySpiller
+    {
+        
+        public static string Spill<T>(T[] targetValues, bool hasWhitespace) => string.Join(hasWhitespace ? ", " : ",", targetValues ?? []);
+        public static string Spill<T>(IList<T> targetValues, bool hasWhitespace) => string.Join(hasWhitespace ? ", " : ",", targetValues ?? []);
+        public static string Spill<T>(IEnumerable<T> targetValues, bool hasWhitespace) => string.Join(hasWhitespace ? ", " : ",", targetValues ?? []);
+        public static string Spill<TKey, TValue>(Dictionary<TKey, TValue> targetValues, bool hasWhitespace) => string.Join(hasWhitespace ? ", " : ",", targetValues.Select(kvp => $"{kvp.Key}:{kvp.Value}"));
+        
+    }
+}
