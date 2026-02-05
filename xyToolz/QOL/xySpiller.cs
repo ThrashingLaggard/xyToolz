96ddb6e134ec04c235428fddc7b2a0684fd3481f
Hansi <test@lol.de>
2026-02-05 05:28:19 +0100

    Added Attributes

--- xyToolz/QOL/xySpiller.cs
+++ xyToolz/QOL/xySpiller.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +9,13 @@ namespace xyToolz.QOL
 {
     public class xySpiller
     {
-        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static string Spill<T>(T[] targetValues, bool hasWhitespace) => string.Join(hasWhitespace ? ", " : ",", targetValues ?? []);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static string Spill<T>(IList<T> targetValues, bool hasWhitespace) => string.Join(hasWhitespace ? ", " : ",", targetValues ?? []);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static string Spill<T>(IEnumerable<T> targetValues, bool hasWhitespace) => string.Join(hasWhitespace ? ", " : ",", targetValues ?? []);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static string Spill<TKey, TValue>(Dictionary<TKey, TValue> targetValues, bool hasWhitespace) => string.Join(hasWhitespace ? ", " : ",", targetValues.Select(kvp => $"{kvp.Key}:{kvp.Value}"));
         
     }
