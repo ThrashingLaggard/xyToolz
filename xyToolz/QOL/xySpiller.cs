b0f356cc526de477e2f35a98eb4742de5ab89fa5
Hansi <test@lol.de>
2026-03-06 17:53:31 +0100

    fix: added  more parameters to the Spilling methods

--- xyToolz/QOL/xySpiller.cs
+++ xyToolz/QOL/xySpiller.cs
@@ -13,13 +13,48 @@ namespace xyToolz.QOL
     public class xySpiller
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static string Spill<T>(T[] targetValues, bool hasWhitespace) => string.Join(hasWhitespace ? ", " : ",", targetValues ?? []);
+        public static string Spill<T>(IEnumerable<T> targetValues, bool hasWhitespace = true, bool? hasSeperator = true, string? seperator = ",") 
+            => Join(targetValues, hasWhitespace, hasSeperator, seperator);
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static string Spill<T>(IList<T> targetValues, bool hasWhitespace) => string.Join(hasWhitespace ? ", " : ",", targetValues ?? []);
+        public static string Spill<T>(T[] targetValues, bool hasWhitespace = true, bool? hasSeperator = true, string? seperator = ",") 
+            => Join(targetValues,hasWhitespace, hasSeperator, seperator);
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static string Spill<T>(IEnumerable<T> targetValues, bool hasWhitespace) => string.Join(hasWhitespace ? ", " : ",", targetValues ?? []);
+        public static string Spill<T>(IList<T> targetValues, bool? hasWhitespace = true, bool? hasSeperator = true, string? seperator = ",")
+            => Join(targetValues,hasWhitespace, hasSeperator, seperator );
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static string Spill<TKey, TValue>(Dictionary<TKey, TValue> targetValues, bool hasWhitespace) => string.Join(hasWhitespace ? ", " : ",", targetValues.Select(kvp => $"{kvp.Key}:{kvp.Value}"));
-        
+        public static string Spill<TKey, TValue>(Dictionary<TKey, TValue> targetValues, bool hasWhitespace = true, bool? hasSeperator = true, string? seperator = ",") 
+            => Join( targetValues.Select(kvp => $"{kvp.Key}:{kvp.Value}"), hasWhitespace, hasSeperator, seperator);
+
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static string Join<T>(IEnumerable<T> values, bool? hasWhitespace = true, bool? hasSeperator = true, string? seperator = ",")
+            => string.Join((((bool)hasSeperator)? seperator: string.Empty) + (((bool)hasWhitespace) ? " " : string.Empty), values);
+
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static string JoinDebug<T>(IEnumerable<T> values, bool? hasWhitespace = true, bool? hasSeperator = true, string? seperator = ",")
+        {
+            string empty = string.Empty;
+            string whiteSpace = " ";
+            
+            if (hasSeperator is false)
+            {
+                seperator = empty;
+            }
+
+            if (hasWhitespace is true)
+            {
+                seperator += whiteSpace;
+            }
+
+            string result = string.Join(seperator, values);
+
+            return result;  
+        }
     }
 }
