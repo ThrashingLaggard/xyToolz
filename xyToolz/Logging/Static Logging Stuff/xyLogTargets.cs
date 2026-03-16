9d531b128b0610cfba0d9fecf0b246e23d72ec76
Hansi <test@lol.de>
2026-03-16 13:47:19 +0100

    Added another useless enum entry

--- xyToolz/Logging/Static Logging Stuff/xyLogTargets.cs
+++ xyToolz/Logging/Static Logging Stuff/xyLogTargets.cs
@@ -1,17 +1,19 @@
 ﻿namespace xyToolz.Helper.Logging
 {
-      /// <summary>
-      ///   Custom target differentiation for the xyLogger:
-      ///   StandardSystemConsole = 0,
-      ///   OwnDebugConsole = 1,
-      ///   RemoteServer = 2,
-      ///   Elsewhere = 3
-      /// </summary>
-      internal enum xyLogTargets
+    /// <summary>
+    ///   Custom target differentiation for the xyLogger:
+    ///   StandardSystemConsole = 0,
+    ///   OwnDebugConsole = 1,
+    ///   RemoteServer = 2,
+    ///   Elsewhere = 3,
+    ///   Everywhere = 4
+    /// </summary>
+    internal enum xyLogTargets
       {
             StandardSystemConsole = 0,
             OwnDebugConsole = 1,
             RemoteServer = 2,
-            Elsewhere = 3
+            Elsewhere = 3,
+            Everywhere = 4
       }
 }
