b41c289c496a4247fd5b538ee2f9cd1b4c7f3505
Leon <thrashing@laggard.de>
2026-04-17 18:11:15 +0200

    consoled the nullability concernes

--- xyToolz/Extensions/xyDictionaryExtension.cs
+++ xyToolz/Extensions/xyDictionaryExtension.cs
@@ -65,13 +65,13 @@ namespace xyToolz.Extensions
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool TryUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue? value) where TKey : notnull
         {
-            ref TValue? val = ref CollectionsMarshal.GetValueRefOrNullRef(dictionary,key);                              // First call to get the reference of the key
+            ref TValue val = ref CollectionsMarshal.GetValueRefOrNullRef(dictionary,key);                              // First call to get the reference of the key
             if (Unsafe.IsNullRef(ref val))
             {
                 return false;
             }
 
-            val = value;                                                                                                                                            // Only using the key's reference, not needing to call the rest of dictionary!!!!
+            val = value!;                                                                                                                                            // Only using the key's reference, not needing to call the rest of dictionary!!!!
             return true;
         }
     }
