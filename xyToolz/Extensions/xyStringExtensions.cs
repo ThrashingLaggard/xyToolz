f543cdd45d6b601103183a4f921deaa9d3de60a3
Leon <thrashing@laggard.de>
2026-05-17 21:21:16 +0200

    fix: Added extension methods

--- xyToolz/Extensions/xyStringExtensions.cs
+++ xyToolz/Extensions/xyStringExtensions.cs
@@ -4,98 +4,114 @@ using xyLogger.Loggers;
 
 namespace xyToolz.Extensions
 {
-        public static class xyStringExtensions
+    public static class xyStringExtensions
+    {
+        /// <summary>
+        /// Repeating a given string a specified number of times.
+        /// </summary>
+        /// <remarks>
+        /// Use char overload for way better performance
+        /// </remarks>
+        /// <param name="text">The text to repeat.</param>
+        /// <param name="count">The end amount.</param>
+        /// <returns>The repeated string.</returns>
+        public static string Repeat(this string text, ushort count)
         {
-                /// <summary>
-                /// Repeating a given string a specified number of times.
-                /// </summary>
-                /// <remarks>
-                /// Use char overload for way better performance
-                /// </remarks>
-                /// <param name="text">The text to repeat.</param>
-                /// <param name="count">The end amount.</param>
-                /// <returns>The repeated string.</returns>
-                public static string Repeat(this string text, ushort count)
-                {
-                    StringBuilder sb_Repeater = new();
-                    for (int i = 0; i < count; i++)
-                    {
-                        sb_Repeater.Append(text);
-                    }
-                    return sb_Repeater.ToString();
-                }
-
-            /// <summary>
-            /// Reverses the characters in a given string.
-            /// </summary>
-            /// <remarks>
-            /// This variant appearently (*Stack Overflow) also reverses the unicode modifiers in character, thus if in doubt, use the Unicode variant of this, utilizing a StringBuilder
-            /// </remarks>
-            /// <param name="input">The string to reverse.</param>
-            /// <returns>The reversed string.</returns>
-            public static string Reverse(this string input)
+            StringBuilder sb_Repeater = new();
+            for (int i = 0; i < count; i++)
             {
-                char[] arr_InputChars = input.ToCharArray();
-                Array.Reverse(arr_InputChars);
-                return new (arr_InputChars);
+                sb_Repeater.Append(text);
             }
+            return sb_Repeater.ToString();
+        }
 
-            /// <summary>
-            /// Reverses the characters in a given string.
-            /// </summary>
-            /// <remarks>
-            /// This variant is appearently safe for usage with unicode
-            /// </remarks>
-            /// <param name="input">The string to reverse.</param>
-            /// <returns>The reversed string.</returns>
-            public static string ReverseUnicode(this string input)
+        /// <summary>
+        /// Reverses the characters in a given string.
+        /// </summary>
+        /// <remarks>
+        /// This variant appearently (*Stack Overflow) also reverses the unicode modifiers in character, thus if in doubt, use the Unicode variant of this, utilizing a StringBuilder
+        /// </remarks>
+        /// <param name="input">The string to reverse.</param>
+        /// <returns>The reversed string.</returns>
+        public static string Reverse(this string input)
+        {
+            char[] arr_InputChars = input.ToCharArray();
+            Array.Reverse(arr_InputChars);
+            return new(arr_InputChars);
+        }
+
+        /// <summary>
+        /// Reverses the characters in a given string.
+        /// </summary>
+        /// <remarks>
+        /// This variant is appearently safe for usage with unicode
+        /// </remarks>
+        /// <param name="input">The string to reverse.</param>
+        /// <returns>The reversed string.</returns>
+        public static string ReverseUnicode(this string input)
+        {
+            StringBuilder reverseBuilder = new(input.Length);
+            for (int i = input.Length - 1; i >= 0; i--)  // unter vorbehalt
             {
-                StringBuilder reverseBuilder = new (input.Length);
-                for (int i = input.Length -1; i >= 0; i--)  // unter vorbehalt
-                {
-                    reverseBuilder.Append(input[i]);
-                }
-                return reverseBuilder.ToString();
+                reverseBuilder.Append(input[i]);
             }
+            return reverseBuilder.ToString();
+        }
 
 
-            /// <summary>
-            /// Converts a UTF-8 string into a byte array.
-            /// </summary>
-            /// <param name="input">The UTF-8 string to convert.</param>
-            /// <returns>A byte array representation of the input string.</returns>
-            public static byte[] ToBytes(this string input)
+        /// <summary>
+        /// Converts a UTF-8 string into a byte array.
+        /// </summary>
+        /// <param name="input">The UTF-8 string to convert.</param>
+        /// <returns>A byte array representation of the input string.</returns>
+        public static byte[] ToBytes(this string input)
+        {
+            try
             {
-                try
-                {
-                    return Encoding.UTF8.GetBytes(input);
-                }
-                catch (Exception ex)
-                {
-                    xyLog.ExLog(ex);
-                    return null!;
-                }
+                return Encoding.UTF8.GetBytes(input);
             }
-
-            /// <summary>
-            /// Converts a Base64-encoded string into a byte array.
-            /// </summary>
-            /// <param name="base64">The Base64-encoded input string.</param>
-            /// <returns>The decoded byte array.</returns>
-            public static byte[] BaseToBytes(this string base64)
+            catch (Exception ex)
             {
-                try
-                {
-                    return Convert.FromBase64String(base64);
-                }
-                catch (Exception ex)
-                {
-                    xyLog.ExLog(ex);
-                    return null!;
-                }
+                xyLog.ExLog(ex);
+                return null!;
             }
+        }
 
+        /// <summary>
+        /// Converts a Base64-encoded string into a byte array.
+        /// </summary>
+        /// <param name="base64">The Base64-encoded input string.</param>
+        /// <returns>The decoded byte array.</returns>
+        public static byte[] BaseToBytes(this string base64)
+        {
+            try
+            {
+                return Convert.FromBase64String(base64);
+            }
+            catch (Exception ex)
+            {
+                xyLog.ExLog(ex);
+                return null!;
+            }
+        }
 
+        /// <summary>
+        /// Indicates whether the specified string is null or an empty string ("").
+        /// </summary>
+        /// <param name="target">The string to test</param>.
+        /// <returns>
+        /// true if the value parameter is null or an empty string (""); otherwise, false.
+        /// </returns>
+        public static bool IsNullOrEmpty(this string target) => (string.IsNullOrEmpty(target)) ? true : false;
+
+        /// <summary>
+        /// Indicates whether the specified string is null or a whitespace
+        /// </summary>
+        /// <param name="target">The string to test</param>.
+        /// <returns>
+        /// true if the value parameter is null or a whitespace; otherwise, false.
+        /// </returns>
+        public static bool IsNullOrWhitespace(this string target) => (string.IsNullOrWhiteSpace(target)) ? true : false;
 
     }
 }
