5b0e557bd39669236347b5074eb838e081cbb4da
Leon <thrashing@laggard.de>
2026-05-18 00:20:43 +0200

    Added extension methods to read components from DateTimeOffset

--- xyToolz/Chrono/xyTime.cs
+++ xyToolz/Chrono/xyTime.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,15 +10,30 @@ namespace xyToolz.Chrono
     /// <summary>
     /// Helper Methods for time operations
     /// </summary>
-    public class xyTime
+    public static class xyTime
     {
         /// <summary>
-        /// Get the hash code for 'DateTime.Now()'
+        /// Get the hash code for 'DateTimeOffset.Now()'
         /// </summary>
         /// <returns>int hashCode</returns>
+         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int HashNow() => DateTimeOffset.Now.GetHashCode();
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static DateTimeOffset Now() => DateTimeOffset.Now;
 
+        /// <summary>
+        /// Returns the UTC DateTime component of the whole 
+        /// </summary>
+        /// <param name="offset"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static DateTime GetDateTime(this DateTimeOffset offset) => offset.UtcDateTime;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static TimeSpan GetOffset(this DateTimeOffset offset) => offset.Offset;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static TimeSpan GetTimeOfDay(this DateTimeOffset offset) => offset.TimeOfDay;
     }
 }
